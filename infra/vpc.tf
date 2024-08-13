df42b90819b783e6fa47748d6b496cad9605a620
jchong <jhchong92@gmail.com>
2024-08-13 19:13:05 +0800

    Connect public subnet to IGW

--- infra/vpc.tf
+++ infra/vpc.tf
@@ -28,3 +28,30 @@ resource "aws_subnet" "private_subnets" {
     Name = "Private Subnet ${count.index + 1}"
   }
 }
+
+resource "aws_internet_gateway" "igw" {
+ vpc_id = aws_vpc.main.id
+ 
+ tags = {
+   Name = "Awesome VPC IG"
+ }
+}
+
+resource "aws_route_table" "public_rt" {
+ vpc_id = aws_vpc.main.id
+ 
+ route {
+   cidr_block = "0.0.0.0/0"
+   gateway_id = aws_internet_gateway.igw.id
+ }
+ 
+ tags = {
+   Name = "Public Route Table"
+ }
+}
+
+resource "aws_route_table_association" "public_subnet_asso" {
+ count = length(var.public_subnet_cidrs)
+ subnet_id      = element(aws_subnet.public_subnets[*].id, count.index)
+ route_table_id = aws_route_table.public_rt.id
+}

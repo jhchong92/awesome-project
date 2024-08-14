acbbd0d59cb66b628d49d144387a220eaa1f5ee8
jchong <jhchong92@gmail.com>
2024-08-14 20:56:26 +0800

    Add CloudFront distribution

---
+++ infra/cloudfront.tf
@@ -0,0 +1,79 @@
+resource "aws_s3_bucket" "main" {
+  bucket = "awesome-oac"
+}
+
+resource "aws_s3_bucket_ownership_controls" "main" {
+  bucket = aws_s3_bucket.main.id
+  rule {
+    object_ownership = "BucketOwnerPreferred"
+  }
+}
+
+resource "aws_s3_bucket_acl" "main" {
+  depends_on = [ aws_s3_bucket_ownership_controls.main ]
+  bucket = aws_s3_bucket.main.id
+  acl    = "private"
+}
+
+resource "aws_cloudfront_origin_access_control" "main" {
+  name       = "awesome-oac"
+  origin_access_control_origin_type = "s3"
+  signing_behavior = "always"
+  signing_protocol = "sigv4"
+}
+
+resource "aws_cloudfront_distribution" "cdn" {
+  enabled             = true
+  default_root_object = "index.html"
+
+  origin {
+    domain_name              = aws_s3_bucket.main.bucket_regional_domain_name
+    origin_access_control_id = aws_cloudfront_origin_access_control.main.id
+    origin_id                = "awesome-s3-origin"
+  }
+
+  origin {
+    domain_name = aws_lb.nlb.dns_name
+    origin_id   = "awesome-nlb-origin"
+
+    custom_origin_config {
+      http_port              = 80
+      https_port             = 443
+      origin_protocol_policy = "http-only"
+      origin_ssl_protocols   = ["TLSv1.2"]
+    }
+
+  }
+
+  default_cache_behavior {
+    target_origin_id       = "awesome-nlb-origin"
+    viewer_protocol_policy = "allow-all"
+
+    allowed_methods = ["GET", "HEAD", "OPTIONS"]
+    cached_methods  = ["GET", "HEAD"]
+
+    forwarded_values {
+      query_string = false
+      cookies {
+        forward = "none"
+      }
+    }
+    min_ttl                = 0
+    default_ttl            = 3600
+    max_ttl                = 86400
+  }
+
+  restrictions {
+    geo_restriction {
+      restriction_type = "none"
+    }
+  }
+
+  viewer_certificate {
+    cloudfront_default_certificate = true
+  }
+
+  tags = {
+    Name = "Awesome Cloudfront Distribution"
+  }
+}

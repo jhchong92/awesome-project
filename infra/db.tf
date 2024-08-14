beae66fed434b80e59d73ba6be658e7c2792efdf
jchong <jhchong92@gmail.com>
2024-08-14 17:28:49 +0800

    Add RDS Security Group

--- infra/db.tf
+++ infra/db.tf
@@ -1,3 +1,26 @@
+resource "aws_security_group" "rds_sg" {
+  name        = "rds_sg"
+  description = "Allow inbound traffic only for MYSQL and all outbound traffic"
+  vpc_id      = aws_vpc.main.id
+  
+  tags = {
+    Name = "RDS Security Group"
+  }
+}
+
+resource "aws_vpc_security_group_egress_rule" "rds_allow_all_traffic_ipv4" {
+  security_group_id = aws_security_group.rds_sg.id
+  cidr_ipv4         = "0.0.0.0/0"
+  ip_protocol       = "-1"
+}
+
+resource "aws_vpc_security_group_ingress_rule" "rds_allow_http_ipv4" {
+  security_group_id = aws_security_group.rds_sg.id
+  cidr_ipv4         = "0.0.0.0/0"
+  from_port         = 3306
+  ip_protocol       = "tcp"
+  to_port           = 3306
+}
 
 resource "aws_db_subnet_group" "db_subnet_group" {
   name       = "main"
@@ -22,6 +45,7 @@ resource "aws_db_instance" "master" {
   password             = var.db_password
   availability_zone    = var.azs[1]
   deletion_protection  = false
+  vpc_security_group_ids = [ aws_security_group.rds_sg.id ]
 
   tags = {
     name = "Awesome MariaDB Master"
@@ -37,6 +61,7 @@ resource "aws_db_instance" "replica" {
   availability_zone    = var.azs[0]
   skip_final_snapshot  = true
   replicate_source_db = aws_db_instance.master.identifier
+  vpc_security_group_ids = [ aws_security_group.rds_sg.id ]
 
   tags = {
     name = "Awesome MariaDB Replica"
